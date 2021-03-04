e92be4ac21300810c72a2656c3c0685010bbd775
Maciej0410 <76994645+Maciej0410@users.noreply.github.com>
2021-03-04 14:45:31 +0100

    Add files via upload

---
+++ zadanie2.py
@@ -0,0 +1,42 @@
+from math import *
+a=input("Podaj pierwsza liczbe: ")
+b=input("Podaj druga liczbe: ")
+
+a=int(a)
+b=int(b)
+
+text='Wynik dodawania: '
+
+print(text)
+print(a+b)
+
+text2='Wynik odejmowania: '
+
+print(text2)
+print(a-b)
+
+text3='Wynik mnozenia: '
+
+print(text3)
+print(a*b)
+
+text4='Wynik dzielenia: '
+
+print(text4)
+print(a/b)
+
+text5='Wynik reszty z dzielenia: '
+
+print(text5)
+print(a%b)
+
+text6='Wyniki potęgowania obydwu liczb do potęgi 2: '
+
+print(text6)
+print(a **2)
+print(b **2)
+
+text7='Wynik pierwiastkowania sumy liczb a i b: '
+
+print(text7)
+print(sqrt(a+b))
